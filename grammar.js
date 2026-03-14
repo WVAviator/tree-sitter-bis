eddab0ec03fea04246e0cbbd7641f73d7f44c950
WVAviator <wvaviator@gmail.com>
2026-03-14 16:48:49 -0500

    add ldv statement

--- grammar.js
+++ grammar.js
@@ -9,9 +9,80 @@
 
 export default grammar({
   name: "bis",
+  extras: (_) => [],
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
-  }
+    source_file: ($) => repeat($._line),
+    _line: ($) => choice($.statement),
+    statement: ($) =>
+      seq(
+        repeat(/\s/),
+        $.stmt_prefix,
+        repeat(/\s/),
+        repeat($._stmt_contents),
+        $.stmt_terminator,
+        $.comment,
+      ),
+    stmt_prefix: (_) => "@",
+    _stmt_contents: ($) => choice($.ldv),
+    stmt_terminator: (_) => ".",
+    separator: ($) => seq(/\\/, $.comment),
+    comment: (_) => prec(-1, seq(/[^\n]*/, /\n/)),
+    variable: ($) => seq("<", /[^>]+/, ">"),
+    variable_type: ($) =>
+      seq(/[afhiosAFHIOS]/, /\d{1,3}/, optional(/\.\d{1,2}/)),
+    _variable_definition: ($) => seq($.variable, optional($.variable_type)),
+    string: ($) => seq("'", /[^']*/, "'"),
+    number: ($) => /\d+/,
+    character: ($) => /./,
+    _value_definition: ($) => choice($.string, $.number, $.variable),
+
+    ldv: ($) => choice($._ldv_fmt_1, $._ldv_fmt_2, $._ldv_fmt_3),
+    ldv_options: $ => seq(',', repeat1(/[CHLOPRUWZchlopruwz]/)),
+    _ldv_fmt_1: ($) =>
+      seq(
+        /[Ll][Dd][Vv]/,
+        optional($.ldv_options),
+        /\s/,
+        seq($._variable_definition, "=", $._value_definition),
+        repeat(seq(",", $._variable_definition, "=", $._value_definition)),
+        /\s/,
+      ),
+    _ldv_fmt_2: ($) =>
+      seq(
+        /[Ll][Dd][Vv]/,
+        $.ldv_options,
+        /\s/,
+        $._variable_definition,
+        repeat(seq(",", $._variable_definition)),
+        /\s/,
+      ),
+    _ldv_fmt_3: ($) =>
+      seq(
+        /[Ll][Dd][Vv]/,
+        ',',
+        alias(/[CHLOPRUWZchlopruwz]*[Qq][CHLOPRUWZchlopruwz]*/, $.ldv_options),
+        /\s/,
+        seq(
+          $._variable_definition,
+          "=",
+          $._value_definition,
+          ",",
+          $.number,
+          optional(seq("(", $.character, ")")),
+        ),
+        repeat(
+          seq(
+            ",",
+            $._variable_definition,
+            "=",
+            $._value_definition,
+            ",",
+            $.number,
+            optional(seq("(", $.character, ")")),
+          ),
+        ),
+        /\s/,
+      ),
+  },
 });
